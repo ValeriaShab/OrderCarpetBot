8160551a6a2b2c303992a30ad04caf221b87462e
ValeriaShab <sabaturavaleria@gmail.com>
2023-02-16 21:49:52 +0200

    first

---
+++ src/main/java/groupId/OrderCarpetBot/OrderCarpetBotApplication.java
@@ -0,0 +1,13 @@
+package groupId.OrderCarpetBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCarpetBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCarpetBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/groupId/OrderCarpetBot/OrderCarpetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package groupId.OrderCarpetBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCarpetBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
